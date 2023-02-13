31a94349bd02e86e5e43f963a48456afab87ce39
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-02-13 16:30:22 -0300

    iniciando useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { useState, useEffect } from 'react'
 import {Card} from './components/card'
 
 export function App() {
-  const [user, setUser] = useState('')
+  const [loginInput, setLoginInput] = useState('')
+  const [user, setUser] = useState([])
+
+  useEffect(() => {}, [])
 
   return (
     <div>
       <header>
         <h1>Digite o login do usuário que deseja visualizar</h1>
-        <h2>{user}</h2>
+        <h2>{loginInput}</h2>
         <div class='divHeader'>
-          <input type="text" placeholder="Escreva o login do usuário" onChange={e => setUser(e.target.value)} />
+          <input type="text" placeholder="Escreva o login do usuário" onChange={e => setLoginInput(e.target.value)} />
           <button type='button' onClick={console.log('sim')}>Pesquisar</button>
         </div>
 
