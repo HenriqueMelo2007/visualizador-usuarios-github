ac2d4ca8fe9fe5c6a2791e4531a078be995e4994
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-02-15 18:26:41 -0300

    corrigindo bugs

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {Card} from './components/card'
 export function App() {
   const [loginInput, setLoginInput] = useState('')
   const [userGitHub, setUserGitHub] = useState('')
-  const [infoUserGitHub, setInfoUserGitHub] = useState()
+  const [infoUserGitHub, setInfoUserGitHub] = useState({local: ''})
 
   function add() {
     setUserGitHub(loginInput)
@@ -13,18 +13,17 @@ export function App() {
   useEffect(() => {
     async function buscandoNaApi () {
       const urlAPI = `https://api.github.com/users/${userGitHub}`
-      const jsonAPI = await fetch(urlAPI).then(u => u.json())
+      const jsonAPI = await fetch(urlAPI).then(u => u.json()).then(data => {
+        setInfoUserGitHub({local: data.location})
+      })
 
-      console.log(jsonAPI)
-
-      setInfoUserGitHub(await jsonAPI)
     }
 
     buscandoNaApi()
   }, [userGitHub])
 
   useEffect(() => {
-    console.log(infoUserGitHub)
+    console.log(infoUserGitHub.local)
   }, [infoUserGitHub])
 
 
@@ -41,7 +40,6 @@ export function App() {
         
       </header>
 
-      
 
     </div>
 
